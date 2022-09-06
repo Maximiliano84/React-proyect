f9bb4d2609f982fce3499a0ff83f4973f8ec8ef8
maxi cassino <maxi_cassino@hotmail.com>
2022-09-06 02:33:20 -0300

    task-React

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import TaskList from "./componentes/TaskList";
+import Taskform from "./componentes/Taskform";
+
+function App() {
+  return (
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10">
+        <Taskform />
+        <TaskList />
+      </div>
+    </main>
+  );
+}
+
+export default App;
